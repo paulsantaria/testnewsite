7e2de4b3caa2b01a698a72c996a8264bb31c07e2
paul santaria <psantaria@hotmail.com>
2020-09-14 22:56:50 -0500

    phptest2

--- php/heyContactForm.php
+++ php/heyContactForm.php
@@ -1,14 +1,14 @@
 <?php
 
 if(isset($_POST['submit'])){
-    $name = $_POST['name']
-    $lastname = $_POST['lastname']
-    $companyname = $_POST['companyname']
-    $email = $_POST['email']
-    $message = $_POST['message']
+    $name = $_POST['name'];
+    $lastname = $_POST['lastname'];
+    $companyname = $_POST['companyname'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-    $mailTo = "hey@lukaymedia.com"
+    $mailTo = "hey@lukaymedia.com";
 
-    mail($mailTo, $name);
-    header("location: index.php?mailsend")
+    mail($mailTo, $name, $lastname, $message);
+    header("location: index.php?mailsend");
 }
