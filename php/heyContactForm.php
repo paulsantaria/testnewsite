70ebc6cbd5c99d168209cd9a30485e3369dafdea
paul santaria <psantaria@hotmail.com>
2020-09-14 22:48:39 -0500

    phptest1

---
+++ php/heyContactForm.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name']
+    $lastname = $_POST['lastname']
+    $companyname = $_POST['companyname']
+    $email = $_POST['email']
+    $message = $_POST['message']
+
+    $mailTo = "hey@lukaymedia.com"
+
+    mail($mailTo, $name);
+    header("location: index.php?mailsend")
+}
